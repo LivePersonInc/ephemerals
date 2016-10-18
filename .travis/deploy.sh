61c948a9be54c69c375ac08adb2185e5f8aa896a
waseemh <waseemh@liveperson.com>
2016-10-18 15:32:04 +0300

    change deployment process to travis ci

---
+++ .travis/deploy.sh
@@ -0,0 +1,6 @@
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+fi
